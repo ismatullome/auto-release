4664fe16447208b3a41608050c2424e27f6b289c
Ismatullo Asatullaev <ismatullome@gmail.com>
2023-09-03 00:47:34 +0500

    feat(release): new template

---
+++ .release-it.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+
+const commitTemplate = fs.readFileSync("template.hbs").toString();
+
+module.exports = {
+  plugins: {
+    "@release-it/conventional-changelog": {
+      infile: "CHANGELOG.md",
+      header: "# Changelog",
+      context: {
+        linkCompare: false,
+      },
+      gitRawCommitsOpts: {
+        merges: null,
+      },
+      parserOpts: {
+        mergePattern: "^Merge pull request #(\\d+) from (.*)$",
+      },
+      preset: {
+        name: "conventionalcommits",
+        types: [
+          {
+            type: "feat",
+            section: "Features",
+          },
+          {
+            type: "fix",
+            section: "Bug Fixes",
+          },
+          {},
+        ],
+      },
+      writerOpts: {
+        commitPartial: commitTemplate,
+      },
+    },
+  },
+};
