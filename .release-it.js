a80cfa0705c0f6daa04c8ac758a5c320bf557a1a
Ismatullo Asatullaev <ismatullome@gmail.com>
2023-09-03 01:09:45 +0500

    fix(template): new template 2

--- .release-it.js
+++ .release-it.js
@@ -1,13 +1,16 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const commitTemplate = fs.readFileSync('template.hbs').toString();
+const commitTemplate = fs.readFileSync("template.hbs").toString();
 
 module.exports = {
+  npm: {
+    publish: false,
+  },
   plugins: {
-    '@release-it/conventional-changelog': {
+    "@release-it/conventional-changelog": {
       writerOpts: {
-        commitPartial: commitTemplate
-      }
-    }
-  }
+        commitPartial: commitTemplate,
+      },
+    },
+  },
 };
